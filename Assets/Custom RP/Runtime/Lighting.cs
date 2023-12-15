13d2d1b50fb2af1abe35c329d25fb8ee49e1a8e6
QiuHan-Desktop <1419091648@qq.com>
2023-12-15 11:50:31 +0800

    Update to [Point and Spot Lights] Spot Lights

--- Assets/Custom RP/Runtime/Lighting.cs
+++ Assets/Custom RP/Runtime/Lighting.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
+using System.Numerics;
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.Rendering;
 using LightType = UnityEngine.LightType;
+using Vector4 = UnityEngine.Vector4;
 
 public class Lighting
 {
@@ -14,7 +17,7 @@ public class Lighting
         name = bufferName
     };
 
-    private const int maxDirLightCount = 4;
+    private const int maxDirLightCount = 4, maxOtherLightCount = 64;
 
     private static int
         dirLightCountId = Shader.PropertyToID("_DirectionalLightCount"),
@@ -26,6 +29,20 @@ public class Lighting
         dirLightColors = new Vector4[maxDirLightCount],
         dirLightDirections = new Vector4[maxDirLightCount],
         dirLightShadowData = new Vector4[maxDirLightCount];
+
+    private static int
+        otherLightCountId = Shader.PropertyToID("_OtherLightCount"),
+        otherLightColorsId = Shader.PropertyToID("_OtherLightColors"),
+        otherLightPositionsId = Shader.PropertyToID("_OtherLightPositions"),
+        //用于Spot Light的Dircection、Angle信息
+        otherLightDirectionsId = Shader.PropertyToID("_OtherLightDirections"),
+        otherLightSpotAnglesId = Shader.PropertyToID("_OtherLightSpotAngles");
+
+    private static Vector4[]
+        otherLightColors = new Vector4[maxOtherLightCount],
+        otherLightPositions = new Vector4[maxOtherLightCount],
+        otherLightDirections = new Vector4[maxOtherLightCount],
+        otherLightSpotAngles = new Vector4[maxOtherLightCount];
             
     private CullingResults cullingResults;
 
@@ -53,32 +70,94 @@ public class Lighting
         // buffer.SetGlobalVector(dirLightDirectionId, -light.transform.forward);
     }
 
+    void SetupPointLight(int index, ref VisibleLight visibleLight)
+    {
+        otherLightColors[index] = visibleLight.finalColor;
+        //将Point Light的Range信息存储在position的w通道中
+        Vector4 position = visibleLight.localToWorldMatrix.GetColumn(3);
+        position.w = 1f / Mathf.Max(visibleLight.range * visibleLight.range, 0.00001f);
+        otherLightPositions[index] = position;
+        //确保Point Light不会被Angle影响
+        otherLightSpotAngles[index] = new Vector4(0f, 1f);
+    }
+    
+    void SetupSpotLight(int index, ref VisibleLight visibleLight)
+    {
+        otherLightColors[index] = visibleLight.finalColor;
+        Vector4 position = visibleLight.localToWorldMatrix.GetColumn(3);
+        position.w = 1f / Mathf.Max(visibleLight.range * visibleLight.range, 0.00001f);
+        otherLightPositions[index] = position;
+        otherLightDirections[index] = -visibleLight.localToWorldMatrix.GetColumn(2);
+
+        Light light = visibleLight.light;
+        float innerCos = Mathf.Cos(Mathf.Deg2Rad * 0.5f * light.innerSpotAngle);
+        float outerCos = Mathf.Cos(Mathf.Deg2Rad * 0.5f * visibleLight.spotAngle);
+        float angleRangeInv = 1f / Mathf.Max(innerCos - outerCos, 0.001f);
+        otherLightSpotAngles[index] = new Vector4(angleRangeInv, -outerCos * angleRangeInv);
+    }
+
     void SetupLights()
     {
         NativeArray<VisibleLight> visibleLights = cullingResults.visibleLights;
         //使用NativeLight获取所有可见的光源，NativeArray提供到Native Memory Buffer的连接，可以高效在C#代码和Unity Native代码之间分享数据
-
-        int dirLightCount = 0;
+        int dirLightCount = 0, otherLightCount = 0;
         for (int i = 0; i < visibleLights.Length; i++)//遍历所有可见光，依次设置数组和索引
         {
             VisibleLight visibleLight = visibleLights[i];
-            if (visibleLight.lightType == LightType.Directional)//筛选Directional Light
+            // if (visibleLight.lightType == LightType.Directional)//筛选Directional Light
+            // {
+            //     SetupDirectionalLight(dirLightCount++, ref visibleLight);
+            //     if (dirLightCount >= maxDirLightCount)
+            //     {
+            //         break;
+            //     }
+            // }
+            switch (visibleLight.lightType)
             {
-                SetupDirectionalLight(dirLightCount++, ref visibleLight);
-                if (dirLightCount >= maxDirLightCount)
-                {
+                case LightType.Directional:
+                    if (dirLightCount < maxDirLightCount)
+                    {
+                        SetupDirectionalLight(dirLightCount++, ref visibleLight);
+                    }
                     break;
-                }
+                case LightType.Point:
+                    if (otherLightCount < maxOtherLightCount)
+                    {
+                        SetupPointLight(otherLightCount++, ref visibleLight);
+                    }
+                    break;
+                case LightType.Spot:
+                    if (otherLightCount < maxOtherLightCount)
+                    {
+                        SetupSpotLight(otherLightCount++, ref visibleLight);
+                    }
+                    break;
             }
         }
-        
+                
         // Debug.Log("VisibleLightCount:" + visibleLights.Length);
         // Debug.Log("DirectionalLightCount:" + dirLightCount);
+        // Debug.Log("OtherLightCount:" + otherLightCount);
         
         buffer.SetGlobalInt(dirLightCountId, visibleLights.Length);
-        buffer.SetGlobalVectorArray(dirLightColorsId, dirLightColors);
-        buffer.SetGlobalVectorArray(dirLightDirectionsId, dirLightDirections);
-        buffer.SetGlobalVectorArray(dirLightShadowDataId, dirLightShadowData);
+        if (dirLightCount > 0)
+            //在有Directional Light的时候才发送光照相关信息给GPU，否则不发送
+        {
+            buffer.SetGlobalVectorArray(dirLightColorsId, dirLightColors);
+            buffer.SetGlobalVectorArray(dirLightDirectionsId, dirLightDirections);
+            buffer.SetGlobalVectorArray(dirLightShadowDataId, dirLightShadowData);
+        }
+
+        buffer.SetGlobalInt(otherLightCountId, otherLightCount);
+        if (otherLightCount > 0)
+        {
+            buffer.SetGlobalVectorArray(otherLightColorsId, otherLightColors);
+            buffer.SetGlobalVectorArray(otherLightPositionsId, otherLightPositions);
+            buffer.SetGlobalVectorArray(otherLightDirectionsId, otherLightDirections);
+            buffer.SetGlobalVectorArray(otherLightSpotAnglesId, otherLightSpotAngles);
+        }
+
+
     }
 
     public void CleanUp()
